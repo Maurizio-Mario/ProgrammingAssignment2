6096307d9a956b22f05a62a650782182609e8442
Maurizio-Mario <maurizio.murino@hotmail.com>
2015-09-23 10:30:03 +0200

    V02, cacheInverse added and apparently working! Test is befor pulling

--- temp_makeChaceMatrix.R
+++ temp_makeChaceMatrix.R
@@ -1,19 +1,36 @@
 
-makeCacheMatrix <- function(x = matrix()) { # Main function
-        get <- function() x # Report the value of x
-        set <- function(y) {  # Eventually change the value of the main function \\\
-                x <<- y       # aka the superior environment of set()  
-                invs <<- NULL #
-        }
-        set_solve <<- function(solve) invs <<- solve # How useless is this part??
-        get_solve <<- function() invs                # Investigate!
-                
+makeCacheMatrix <- function(x = matrix()) {
+        invs <<- NULL
+        set <- function(y) {                
+                x <<- y                      
+                invs <<- NULL
+                }
+        get <- function() x 
+        set_solve <<- function(inverse) invs <<- inverse 
+        get_solve <<- function() invs               
         list(get = get, set = set, set_solve = set_solve, get_solve = get_solve)
 }
 
-a <- makeCacheMatrix()
-a$get()
-a$set(matrix(rnorm(16, 8, 4), 4, 4))
-a$set_solve(10)
-a$get_solve()
+
+# The second function
+cacheSolve <- function(x, ...)    {  # It takes as argument the object where the first is stored
+        invs <- x$get_solve()
+        if(!is.null(invs))    {
+                message("getting cached data")
+                return(invs)
+        }
+        dt <- x$get()
+        invs <- solve(dt)
+        x$set_solve(invs)
+        invs
+}
+
+x = matrix(rnorm(10^6, 5^6, 2^6), 1000, 1000)
+m = makeCacheMatrix(x)
+m$get()
+
+cacheSolve(m)
+
+rm(list = ls())
+
 
